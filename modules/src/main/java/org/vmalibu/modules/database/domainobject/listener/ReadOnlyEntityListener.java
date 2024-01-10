b5f5a07d77e6f5b3afe03869e18a56ad25285439
V. Geroev <rebytichkin@gmail.com>
2024-01-10 19:46:08 +0300

    fix: fix

--- modules/src/main/java/org/vmalibu/modules/database/domainobject/listener/ReadOnlyEntityListener.java
+++ modules/src/main/java/org/vmalibu/modules/database/domainobject/listener/ReadOnlyEntityListener.java
@@ -1,6 +1,5 @@
 package org.vmalibu.modules.database.domainobject.listener;
 
-import jakarta.persistence.PostLoad;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreRemove;
 import jakarta.persistence.PreUpdate;
@@ -11,7 +10,6 @@ public class ReadOnlyEntityListener {
     @PrePersist
     @PreUpdate
     @PreRemove
-    @PostLoad
     private void fireException(DomainObject domainObject) {
         throw new UnsupportedOperationException("Domain object (name: %s) is readonly".formatted(domainObject.getClass()));
     }
