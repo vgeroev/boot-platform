212741ce329587152f35880bf6eab8f7fd24f4ac
V. Geroev <rebytichkin@gmail.com>
2024-05-16 18:48:10 +0300

    feat: added possibility to get username of authenticated user

--- security/src/main/java/org/vmalibu/module/security/argumentresolver/UserSourceHandlerMethodArgumentResolver.java
+++ security/src/main/java/org/vmalibu/module/security/argumentresolver/UserSourceHandlerMethodArgumentResolver.java
@@ -4,11 +4,14 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.vmalibu.module.security.authorization.source.UserSource;
+
+import java.util.Objects;
 
 public class UserSourceHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -26,7 +29,12 @@ public class UserSourceHandlerMethodArgumentResolver implements HandlerMethodArg
         return getUserSource(authentication);
     }
 
+    // TODO Kostyl' :) Need to be refactored.
     private UserSource getUserSource(Authentication authentication) {
+        if (!(authentication instanceof JwtAuthenticationToken jwtAuthenticationToken)) {
+            throw new IllegalStateException("Unexpected authentication " + authentication.getClass());
+        }
+
         return new UserSource() {
             @Override
             public @NonNull String getUserId() {
@@ -34,9 +42,12 @@ public class UserSourceHandlerMethodArgumentResolver implements HandlerMethodArg
             }
 
             @Override
-            public @NonNull Authentication getAuthentication() {
-                return authentication;
+            public @NonNull String getUsername() {
+                return Objects.requireNonNull(
+                        jwtAuthenticationToken.getTokenAttributes().get("preferred_username")
+                ).toString();
             }
+
         };
     }
 }
--- security/src/main/java/org/vmalibu/module/security/authorization/source/UserSource.java
+++ security/src/main/java/org/vmalibu/module/security/authorization/source/UserSource.java
@@ -1,11 +1,11 @@
 package org.vmalibu.module.security.authorization.source;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.springframework.security.core.Authentication;
 
 public interface UserSource {
 
     @NonNull String getUserId();
 
-    @NonNull Authentication getAuthentication();
+    @NonNull String getUsername();
+
 }
