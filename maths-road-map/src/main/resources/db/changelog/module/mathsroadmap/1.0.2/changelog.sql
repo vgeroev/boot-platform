95ba341e9cd3176cf3d84976d0c75ad83e1ec743
V. Geroev <rebytichkin@gmail.com>
2024-12-06 22:58:28 +0300

    build: 1.0.4

--- maths-road-map/src/main/resources/db/changelog/module/mathsroadmap/1.0.2/changelog.sql
+++ maths-road-map/src/main/resources/db/changelog/module/mathsroadmap/1.0.2/changelog.sql
@@ -1,3 +1,3 @@
 --changeset vmalibu:1.0.2
 UPDATE maths_road_map_article SET description = '' WHERE description IS NULL;
-CREATE INDEX index_title_description_trgm ON maths_road_map_article using gin ((title || ' ' || description) gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS index_title_description_trgm ON maths_road_map_article using gin ((title || ' ' || description) gin_trgm_ops);
