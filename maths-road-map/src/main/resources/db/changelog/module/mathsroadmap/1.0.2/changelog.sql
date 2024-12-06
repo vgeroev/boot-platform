52315a053cfe6617735ab9015c7a251315c58f2c
V. Geroev <rebytichkin@gmail.com>
2024-12-06 22:33:30 +0300

    feat: added comment to changelog 1.0.2

--- maths-road-map/src/main/resources/db/changelog/module/mathsroadmap/1.0.2/changelog.sql
+++ maths-road-map/src/main/resources/db/changelog/module/mathsroadmap/1.0.2/changelog.sql
@@ -1,2 +1,3 @@
+--changeset vmalibu:1.0.2
 UPDATE maths_road_map_article SET description = '' WHERE description IS NULL;
 CREATE INDEX index_title_description_trgm ON maths_road_map_article using gin ((title || ' ' || description) gin_trgm_ops);
